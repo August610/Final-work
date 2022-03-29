ba2bd7116a5e1e906a4e18c24b60bc36c9c3a902
August610 <54235530+August610@users.noreply.github.com>
2022-03-29 17:43:38 +0300

    Create PostPage.jsx
    Добавлена страница для отображения конкретного поста

---
+++ src/pages/PostPage/PostPage.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import api from "../../utils/Api";
+import { useParams } from "react-router-dom";
+import { Post } from '../../components/Post/Post';
+
+export const PagePost = ({currentUser, isLoading, handlePostLike}) => {
+  const [post, setpost] = useState([]);
+  const { postID } = useParams();
+;
+
+
+  useEffect(()=> {
+    api.getPostById(postID)
+      .then((postData)=> {
+        console.log(postData);
+        setpost(postData);
+      })
+  },[])
+
+  return (
+    <>
+          {/* {isLoading  && <Spinner/> } */}
+          <Post {...post} currentUser={currentUser} onPostLike={handlePostLike}/>  
+    </>
+  );
+};
