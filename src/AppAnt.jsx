7a990c3163a88b8a12a20bd6e503f4e984ed1c48
August610 <54235530+August610@users.noreply.github.com>
2022-04-03 17:01:40 +0300

    Update AppAnt.jsx
    изменение запроса на лайки

--- src/AppAnt.jsx
+++ src/AppAnt.jsx
@@ -8,12 +8,13 @@ import { DeletePostContext } from "./context/deletePostContext";
 import { Route, Routes } from "react-router-dom";
 import { AllPosts } from "./pages/AllPostsPage/AllPostsPage";
 import { PagePost } from "./pages/PostPage/PostPage";
+import { AppContext } from "./context/appContext";
 
 
 export const AppAnt = () => {
   const [cards, setCards] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
-  
+
 
   const reload = () => {
     window.location.reload();
@@ -40,51 +41,53 @@ export const AppAnt = () => {
       .catch(alert("Ошибка доступа"))
   }
 
-  function handlePostLike({ _id, likes }) {
-    const isLiked = likes.some(id => id === currentUser._id)
-    api.changeLikeStatus(_id, isLiked)
-      .then((newCard) => {
-        const newCardsState = cards.map(c => {
-          return c._id === newCard._id ? newCard : c
-        })
-
-        setCards(newCardsState);
-      })
+  function handlePostLike(_id, isLiked) {
+    console.log(isLiked)
+    api.changeLikeStatus(_id, isLiked).then((newCard) => {
+      const newCardsState = cards.map((c) => {
+        return c._id === newCard._id ? newCard : c;
+      });
+      setCards(newCardsState);
+    });
   }
 
 
+
   return (
-    <DeletePostContext.Provider value={handleDeletePost}>
-      <CurrentUserContext.Provider value={currentUser}>
-        <Header user={currentUser} onUpdateUser={handleUpdateUser}>
-          <Logo onClick={reload} />
-        </Header>
-        <main className="content container">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <AllPosts
-                  currentUser={currentUser}
-                  cards={cards}
-                  handlePostLike={handlePostLike}
-                />
-              }
-            />
-            <Route
-              path="/posts/:postID"
-              element={
-                <PagePost
-                  currentUser={currentUser}            
-                  handlePostLike={handlePostLike}
-                />
-              }
-            />
-            <Route path="*" element={<h1>Страница не найдена</h1>} />
-          </Routes>
-        </main>
-        <Footer>© You!</Footer>
-      </CurrentUserContext.Provider>
-    </DeletePostContext.Provider>
+    <AppContext.Provider value={{handlePostLike}}>
+      <DeletePostContext.Provider value={handleDeletePost}>
+        <CurrentUserContext.Provider value={currentUser}>
+          <Header user={currentUser} onUpdateUser={handleUpdateUser}>
+            <Logo onClick={reload} />
+          </Header>
+          <main className="content container">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <AllPosts
+                    currentUser={currentUser}
+                    cards={cards}
+                    // handlePostLike={handlePostLike}
+                  />
+                }
+              />
+              <Route
+                path="/posts/:postID"
+                element={
+                  <PagePost
+                    currentUser={currentUser}
+                    onPostLike={handlePostLike}
+                    cards={cards}
+                  />
+                }
+              />
+              <Route path="*" element={<h1>Страница не найдена</h1>} />
+            </Routes>
+          </main>
+          <Footer>© You!</Footer>
+        </CurrentUserContext.Provider>
+      </DeletePostContext.Provider>
+    </AppContext.Provider>
   );
 };
