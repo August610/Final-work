1d5b1edefb787f43f32721f13ffb01eff2fceed2
August610 <54235530+August610@users.noreply.github.com>
2022-04-08 18:26:27 +0300

    Update AppAnt.jsx
    добавлен контекст для обновления карточки и функция обновления

--- src/AppAnt.jsx
+++ src/AppAnt.jsx
@@ -10,6 +10,7 @@ import { AllPosts } from "./pages/AllPostsPage/AllPostsPage";
 import { PagePost } from "./pages/PostPage/PostPage";
 import { AppContext } from "./context/appContext";
 import { CreatePost } from "./pages/CreatePost/CreatePost";
+import { UpdatePostContext } from "./context/updatePostContext";
 
 
 export const AppAnt = () => {
@@ -47,6 +48,10 @@ export const AppAnt = () => {
       });
   }
 
+  // console.log(cards.indexOf(
+  //   cards.find(e => e._id === "622bda6106c7d323b8ae4618")));
+
+
   function handlePostLike(_id, isLiked) {
     // console.log(isLiked)
     api.changeLikeStatus(_id, isLiked).then((newCard) => {
@@ -65,47 +70,61 @@ export const AppAnt = () => {
       });
   };
 
+  function handleUpdatePost(data, id) {
+    api.updatePost(data, id)
+      .then((newCard) => {
+        // cards.splice(cards.indexOf(cards.find(e => e.id === id)), 1, updatePost)
+        // setCards(cards);
+        const newCards = cards.map((c) => { 
+          return c._id === newCard._id ? newCard : c 
+        });
+        setCards(newCards);
+      });
+  }
+
   return (
-    <AppContext.Provider value={{ handlePostLike, isLoading }}>
-      <DeletePostContext.Provider value={handleDeletePost}>
-        <CurrentUserContext.Provider value={currentUser}>
-          <Header user={currentUser} onUpdateUser={handleUpdateUser}>
-            <Logo onClick={reload} />
-          </Header>
-          <main className="content container">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <AllPosts
-                    currentUser={currentUser}
-                    cards={cards}
-                    loading={isLoading}
-                  />
-                }
-              />
-              <Route
-                path="/posts/:postID"
-                element={
-                  <PagePost
-                    currentUser={currentUser}
-                    onPostLike={handlePostLike}
-                    cards={cards}
-                  />
-                }
-              />
-              <Route
-                path="/createPost"
-                element={
-                  <CreatePost handleCreateNewPost={handleCreateNewPost} />
-                }
-              />
-              <Route path="*" element={<h1>Страница не найдена</h1>} />
-            </Routes>
-          </main>
-          <Footer>© You!</Footer>
-        </CurrentUserContext.Provider>
-      </DeletePostContext.Provider>
-    </AppContext.Provider>
+    <UpdatePostContext.Provider value={handleUpdatePost}>
+      <AppContext.Provider value={{ handlePostLike, isLoading }}>
+        <DeletePostContext.Provider value={handleDeletePost}>
+          <CurrentUserContext.Provider value={currentUser}>
+            <Header user={currentUser} onUpdateUser={handleUpdateUser}>
+              <Logo onClick={reload} />
+            </Header>
+            <main className="content container">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <AllPosts
+                      currentUser={currentUser}
+                      cards={cards}
+                      loading={isLoading}
+                    />
+                  }
+                />
+                <Route
+                  path="/posts/:postID"
+                  element={
+                    <PagePost
+                      currentUser={currentUser}
+                      onPostLike={handlePostLike}
+                      cards={cards}
+                    />
+                  }
+                />
+                <Route
+                  path="/createPost"
+                  element={
+                    <CreatePost handleCreateNewPost={handleCreateNewPost} />
+                  }
+                />
+                <Route path="*" element={<h1>Страница не найдена</h1>} />
+              </Routes>
+            </main>
+            <Footer>© You!</Footer>
+          </CurrentUserContext.Provider>
+        </DeletePostContext.Provider>
+      </AppContext.Provider>
+    </UpdatePostContext.Provider>
   );
 };
