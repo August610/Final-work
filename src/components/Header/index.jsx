831df89adced866ffc1877f35d2477268b30b159
August610 <54235530+August610@users.noreply.github.com>
2022-04-11 14:49:43 +0300

    Update index.jsx
    общие исправления

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import cn from "classnames";
 import s from "./styles.module.css";
+import { Link } from "react-router-dom";
 
 
-export const Header = ({children, user}) => {
+export const Header = ({ children, user }) => {
   return (
-      <header className={s.header}>
-        <div className={cn(s['header-wrapper'], 'container')}>
-            {children}
-          <div className={s.profile}>
-            <span>{user.email}</span>
-            <span>{user.name}</span>
-          </div>
+
+    <header className={s.header}>
+      <div className={cn(s['header-wrapper'], 'container')}>
+        {children}
+        <div className={s.profile}>
+          <span>{user.email}</span>
+          <span>{user.name}</span>
         </div>
-      </header>
+      </div>
+    </header>
   );
 };
