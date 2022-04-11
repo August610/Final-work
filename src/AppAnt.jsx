1173233b9bc9b76185fe72effc02245095ef2f82
August610 <54235530+August610@users.noreply.github.com>
2022-04-11 14:49:34 +0300

    Update AppAnt.jsx
    новый контекст и функция добавления комментария

--- src/AppAnt.jsx
+++ src/AppAnt.jsx
@@ -5,12 +5,13 @@ import { Logo } from "./components/Logo";
 import api from "./utils/Api";
 import { CurrentUserContext } from "./context/currentUserContext";
 import { DeletePostContext } from "./context/deletePostContext";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { AllPosts } from "./pages/AllPostsPage/AllPostsPage";
 import { PagePost } from "./pages/PostPage/PostPage";
 import { AppContext } from "./context/appContext";
 import { CreatePost } from "./pages/CreatePost/CreatePost";
 import { UpdatePostContext } from "./context/updatePostContext";
+import { AddCommentContext } from "./context/commentContext";
 
 
 export const AppAnt = () => {
@@ -48,12 +49,8 @@ export const AppAnt = () => {
       });
   }
 
-  // console.log(cards.indexOf(
-  //   cards.find(e => e._id === "622bda6106c7d323b8ae4618")));
-
 
   function handlePostLike(_id, isLiked) {
-    // console.log(isLiked)
     api.changeLikeStatus(_id, isLiked).then((newCard) => {
       const newCardsState = cards.map((c) => {
         return c._id === newCard._id ? newCard : c;
@@ -75,56 +72,72 @@ export const AppAnt = () => {
       .then((newCard) => {
         // cards.splice(cards.indexOf(cards.find(e => e.id === id)), 1, updatePost)
         // setCards(cards);
-        const newCards = cards.map((c) => { 
-          return c._id === newCard._id ? newCard : c 
+        const newCards = cards.map((c) => {
+          return c._id === newCard._id ? newCard : c
+        });
+        setCards(newCards);
+      });
+  }
+
+  function handleAddComment(data, id) {
+    api.addComments(data, id)
+      .then((newCard) => {
+        // cards.splice(cards.indexOf(cards.find(e => e.id === id)), 1, updatePost)
+        // setCards(cards);
+        const newCards = cards.map((c) => {
+          return c._id === newCard._id ? newCard : c
         });
         setCards(newCards);
       });
   }
 
   return (
-    <UpdatePostContext.Provider value={handleUpdatePost}>
-      <AppContext.Provider value={{ handlePostLike, isLoading }}>
-        <DeletePostContext.Provider value={handleDeletePost}>
-          <CurrentUserContext.Provider value={currentUser}>
-            <Header user={currentUser} onUpdateUser={handleUpdateUser}>
-              <Logo onClick={reload} />
-            </Header>
-            <main className="content container">
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <AllPosts
-                      currentUser={currentUser}
-                      cards={cards}
-                      loading={isLoading}
-                    />
-                  }
-                />
-                <Route
-                  path="/posts/:postID"
-                  element={
-                    <PagePost
-                      currentUser={currentUser}
-                      onPostLike={handlePostLike}
-                      cards={cards}
-                    />
-                  }
-                />
-                <Route
-                  path="/createPost"
-                  element={
-                    <CreatePost handleCreateNewPost={handleCreateNewPost} />
-                  }
-                />
-                <Route path="*" element={<h1>Страница не найдена</h1>} />
-              </Routes>
-            </main>
-            <Footer>© You!</Footer>
-          </CurrentUserContext.Provider>
-        </DeletePostContext.Provider>
-      </AppContext.Provider>
-    </UpdatePostContext.Provider>
+    <AddCommentContext.Provider value={handleAddComment}>
+      <UpdatePostContext.Provider value={handleUpdatePost}>
+        <AppContext.Provider value={{ handlePostLike, isLoading }}>
+          <DeletePostContext.Provider value={handleDeletePost}>
+            <CurrentUserContext.Provider value={currentUser}>
+              <Header user={currentUser} onUpdateUser={handleUpdateUser}>
+                {/* <Link to={"/"}> */}
+                  <Logo />
+                {/* </Link> */}
+              </Header>
+              <main className="content container">
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <AllPosts
+                        currentUser={currentUser}
+                        cards={cards}
+                        loading={isLoading}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/posts/:postID"
+                    element={
+                      <PagePost
+                        currentUser={currentUser}
+                        onPostLike={handlePostLike}
+                        cards={cards}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/createPost"
+                    element={
+                      <CreatePost handleCreateNewPost={handleCreateNewPost} />
+                    }
+                  />
+                  <Route path="*" element={<h1>Страница не найдена</h1>} />
+                </Routes>
+              </main>
+              <Footer>© You!</Footer>
+            </CurrentUserContext.Provider>
+          </DeletePostContext.Provider>
+        </AppContext.Provider>
+      </UpdatePostContext.Provider>
+    </AddCommentContext.Provider>
   );
 };
