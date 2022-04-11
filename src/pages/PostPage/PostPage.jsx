cba2b0d67c9788fc4e76ecf2a7f56efb6cafeb21
August610 <54235530+August610@users.noreply.github.com>
2022-04-11 14:46:58 +0300

    Update PostPage.jsx
    стейты на пост

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import api from "../../utils/Api";
 import { useParams } from "react-router-dom";
 import { Post } from '../../components/Post/Post';
@@ -8,17 +8,33 @@ import ContentLoader from "react-content-loader";
 
 export const PagePost = ({ cards }) => {
   const { postID } = useParams();
-  ;
+  const [postData, setPostData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
 
-  const handler = useCallback(() => {
-    return api.getPostById(postID)
-  }, [postID, cards]);
+  // const handler = useCallback(() => {
+  //   return api.getPostById(postID)
+  // }, [postID, cards]);
 
-  const { data: post, loading, error } = useApi(handler);
+  // const { data: post, loading, error } = useApi(handler);
+  // return (
 
+  useEffect(() => {
+    setIsLoading(true)
+    api.getPostById(postID)
+      .then((dataPost) => {
+        setPostData(dataPost)
+      })
+      // .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false))
+  }, [postID, cards])
+
+  // function handleFormReviews(dataProduct){
+  //   setProductData(dataProduct)
+  // }
   return (
     <>
-      {/* {loading && <ContentLoader
+      {/* {isLoading && <ContentLoader
         viewBox="0 0 400 160"
         height={160}
         width={400}
@@ -29,7 +45,8 @@ export const PagePost = ({ cards }) => {
         <circle cx="194" cy="86" r="8" />
         <circle cx="238" cy="86" r="8" />
       </ContentLoader>} */}
-      {post && <Post {...post} />}
+      {/* {isLoading  && <Spinner/> } */}
+      {postData && <Post {...postData}/>}
     </>
   );
 };
